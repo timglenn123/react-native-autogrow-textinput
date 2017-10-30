e4d5a45fe54e5c22f32b1b8ccab3eac6b33ca741
Tim Glenn <timglenn@nimbusblue.com>
2017-10-30 14:06:01 -0600

    added prop types

--- src/AutoGrowingTextInput.js
+++ src/AutoGrowingTextInput.js
@@ -1,8 +1,14 @@
-import React, {Component, PropTypes} from 'react';
-import ReactNative, {View, TextInput, LayoutAnimation, Platform, NativeModules} from 'react-native';
-
-const ANDROID_PLATFORM = (Platform.OS === 'android');
-const IOS_PLATFORM = (Platform.OS === 'ios');
+import React, { Component } from "react";
+import ReactNative, {
+  View,
+  TextInput,
+  LayoutAnimation,
+  Platform,
+  NativeModules
+} from "react-native";
+import PropTypes from "prop-types";
+const ANDROID_PLATFORM = Platform.OS === "android";
+const IOS_PLATFORM = Platform.OS === "ios";
 const DEFAULT_ANIM_DURATION = 100;
 
 const AutoGrowTextInputManager = NativeModules.AutoGrowTextInputManager;
@@ -11,7 +17,9 @@ export default class AutoGrowingTextInput extends Component {
   constructor(props) {
     super(props);
 
-    this._onContentSizeChangeAndroid = this._onContentSizeChangeAndroid.bind(this);
+    this._onContentSizeChangeAndroid = this._onContentSizeChangeAndroid.bind(
+      this
+    );
     this._onChangeAndroid = this._onChangeAndroid.bind(this);
     this._onChangeIOS = this._onChangeIOS.bind(this);
     this._onContentSizeChangeIOS = this._onContentSizeChangeIOS.bind(this);
@@ -23,7 +31,7 @@ export default class AutoGrowingTextInput extends Component {
   }
 
   componentDidMount() {
-    if(this.shouldApplyNativeIOSSettings()) {
+    if (this.shouldApplyNativeIOSSettings()) {
       const reactTag = this.textInputReactTag();
       if (reactTag) {
         AutoGrowTextInputManager.applySettingsForInput(reactTag, {
@@ -35,7 +43,7 @@ export default class AutoGrowingTextInput extends Component {
   }
 
   componentWillUnmount() {
-    if(this.shouldApplyNativeIOSSettings()) {
+    if (this.shouldApplyNativeIOSSettings()) {
       const reactTag = this.textInputReactTag();
       if (reactTag) {
         AutoGrowTextInputManager.performCleanupForInput(reactTag);
@@ -44,7 +52,12 @@ export default class AutoGrowingTextInput extends Component {
   }
 
   shouldApplyNativeIOSSettings() {
-    return IOS_PLATFORM && AutoGrowTextInputManager && (this.props.disableScrollAndBounceIOS || this.props.enableScrollToCaretIOS);
+    return (
+      IOS_PLATFORM &&
+      AutoGrowTextInputManager &&
+      (this.props.disableScrollAndBounceIOS ||
+        this.props.enableScrollToCaretIOS)
+    );
   }
 
   textInputReactTag() {
@@ -55,15 +68,21 @@ export default class AutoGrowingTextInput extends Component {
 
   _renderTextInputAndroid() {
     return (
-      <View style={{flex: 1, flexDirection: 'row'}}>
-      <TextInput
-        multiline={true}
-        {...this.props} {...this.style}
-        style={[this.props.style, {height: this._getValidHeight(this.state.height), flex: 1}]}
-        onContentSizeChange={this._onContentSizeChangeAndroid}
-        onChange={this._onChangeAndroid}
-        ref={(r) => { this._textInput = r; }}
-      />
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <TextInput
+          multiline={true}
+          {...this.props}
+          {...this.style}
+          style={[
+            this.props.style,
+            { height: this._getValidHeight(this.state.height), flex: 1 }
+          ]}
+          onContentSizeChange={this._onContentSizeChangeAndroid}
+          onChange={this._onChangeAndroid}
+          ref={r => {
+            this._textInput = r;
+          }}
+        />
       </View>
     );
   }
@@ -73,16 +92,20 @@ export default class AutoGrowingTextInput extends Component {
       <TextInput
         multiline={true}
         {...this.props}
-        style={[this.props.style, {height: 'auto'}]}
+        style={[this.props.style, { height: "auto" }]}
         onContentSizeChange={this._onContentSizeChangeIOS}
         onChange={this._onChangeIOS}
-        ref={(r) => { this._textInput = r; }}
+        ref={r => {
+          this._textInput = r;
+        }}
       />
     );
   }
 
   render() {
-    return ANDROID_PLATFORM ? this._renderTextInputAndroid() : this._renderTextInputIOS();
+    return ANDROID_PLATFORM
+      ? this._renderTextInputAndroid()
+      : this._renderTextInputIOS();
   }
 
   /*
@@ -93,8 +116,8 @@ export default class AutoGrowingTextInput extends Component {
    using a flag (androidFirstContentSizeChange) to pervent multiple updates in case both notifications works simultaniously in some cases
    */
   _onContentSizeChangeAndroid(event) {
-    if(this.state.androidFirstContentSizeChange) {
-      this.setState({androidFirstContentSizeChange: false});
+    if (this.state.androidFirstContentSizeChange) {
+      this.setState({ androidFirstContentSizeChange: false });
       this._handleNativeEvent(event.nativeEvent);
     }
     if (this.props.onContentSizeChange) {
@@ -103,7 +126,7 @@ export default class AutoGrowingTextInput extends Component {
   }
 
   _onChangeAndroid(event) {
-    if(!this.state.androidFirstContentSizeChange) {
+    if (!this.state.androidFirstContentSizeChange) {
       this._handleNativeEvent(event.nativeEvent);
     }
     if (this.props.onChange) {
@@ -112,7 +135,7 @@ export default class AutoGrowingTextInput extends Component {
   }
 
   _onContentSizeChangeIOS(event) {
-    if(this.props.onContentSizeChange) {
+    if (this.props.onContentSizeChange) {
       this.props.onContentSizeChange(event);
     }
   }
@@ -120,14 +143,14 @@ export default class AutoGrowingTextInput extends Component {
   _onChangeIOS(event) {
     this._animateIfNecessary();
 
-    if(this.props.onChange) {
+    if (this.props.onChange) {
       this.props.onChange(event);
     }
   }
 
   _getValidHeight(height) {
     const minCappedHeight = Math.max(this.props.minHeight, height);
-    if(this.props.maxHeight == null) {
+    if (this.props.maxHeight == null) {
       return minCappedHeight;
     }
     return Math.min(this.props.maxHeight, minCappedHeight);
@@ -136,7 +159,10 @@ export default class AutoGrowingTextInput extends Component {
   _animateIfNecessary() {
     if (this.props.animation.animated) {
       const duration = this.props.animation.duration || DEFAULT_ANIM_DURATION;
-      LayoutAnimation.configureNext({...LayoutAnimation.Presets.easeInEaseOut, duration: duration});
+      LayoutAnimation.configureNext({
+        ...LayoutAnimation.Presets.easeInEaseOut,
+        duration: duration
+      });
     }
   }
 
@@ -144,13 +170,21 @@ export default class AutoGrowingTextInput extends Component {
     let newHeight = this.state.height;
     if (nativeEvent.contentSize && this.props.autoGrowing) {
       newHeight = nativeEvent.contentSize.height;
-      if (this.state.height !== newHeight && newHeight <= this.props.maxHeight && this.props.onHeightChanged) {
-        this.props.onHeightChanged(newHeight, this.state.height, newHeight - this.state.height);
+      if (
+        this.state.height !== newHeight &&
+        newHeight <= this.props.maxHeight &&
+        this.props.onHeightChanged
+      ) {
+        this.props.onHeightChanged(
+          newHeight,
+          this.state.height,
+          newHeight - this.state.height
+        );
       }
     }
 
     this._animateIfNecessary();
-    this.setState({height: newHeight});
+    this.setState({ height: newHeight });
   }
 
   setNativeProps(nativeProps = {}) {
@@ -158,7 +192,7 @@ export default class AutoGrowingTextInput extends Component {
   }
 
   resetHeightToMin() {
-    this.setState({height: this.props.minHeight});
+    this.setState({ height: this.props.minHeight });
   }
 
   clear() {
@@ -191,14 +225,14 @@ AutoGrowingTextInput.propTypes = {
   onChange: PropTypes.func,
   animation: PropTypes.object,
   disableScrollAndBounceIOS: PropTypes.bool,
-  enableScrollToCaretIOS: PropTypes.bool,
+  enableScrollToCaretIOS: PropTypes.bool
 };
 AutoGrowingTextInput.defaultProps = {
   autoGrowing: true,
   minHeight: 35,
   initialHeight: 35,
   maxHeight: null,
-  animation: {animated: false, duration: DEFAULT_ANIM_DURATION},
+  animation: { animated: false, duration: DEFAULT_ANIM_DURATION },
   disableScrollAndBounceIOS: false,
-  enableScrollToCaretIOS: false,
+  enableScrollToCaretIOS: false
 };
